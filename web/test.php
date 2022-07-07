4f85977cb9058059fcb7fa32b93844437d0681e5
ezenia22 <20011155@myrp.edu.sg>
2022-07-07 12:38:32 +0800

    Updates

--- web/test.php
+++ web/test.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="viewport" content="width=device-wdith, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Welcome to Lesson 8 of C339">
     <title>Testing</title>
 </head>
 <body>
